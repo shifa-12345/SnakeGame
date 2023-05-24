fad5494f9829e131315e81cb3f28223e3f754d13
Shifa Shaikh <133372928+shifa-12345@users.noreply.github.com>
2023-05-24 19:46:44 +0530

    Add files via upload

---
+++ SnakeGame/javascript/index.js
@@ -0,0 +1,122 @@
+let inputDir={x:0,y:0};
+const foodSound =new Audio('../music/food.mp3');
+const gameOver=new Audio('../music/gameover.mp3');
+const moveSound=new Audio('../music/move.mp3');
+const musicSound= new Audio('../music/music.mp3');
+let score=0;
+let speed=5;
+let lastPaintTime=0;
+let snakeArr=[
+    {x:13,y:15}
+]
+let food={ x:6,y:7}
+//game functions
+
+function main(ctime){
+    window.requestAnimationFrame(main);
+   // console.log(ctime);
+    if((ctime-lastPaintTime)/1000 <1/speed){
+     return;
+    }
+       lastPaintTime=ctime;
+       gameEngine();
+}
+function isCollide(snake){
+   
+ // if bump into yourself
+
+ for(let i=1; i<snakeArr.length;i++){
+    if(snake[i].x===snake[0].x && snake[i].y ===snake[0].y){
+        return true;
+    }
+ }
+ // if snake bump into the wall
+ if(snake[0].x >=18 || snake[0].x<=0 || snake[0].y >=18 || snake[0].y<=0){
+    return true;
+ }
+}
+function gameEngine(){
+    // part 1 : updating the snake array
+if(isCollide(snakeArr)){
+    gameOver.play();
+    musicSound.pause();
+    inputDir ={x:0,y:0};
+    alert("Game over.Press any key to play Again!!")
+    snakeArr=[{x:13,y:15}];
+    musicSound.play();
+    score=0;
+}
+ // if u have eten the food ,increment the score and regenarate the food.
+   if(snakeArr[0].y===food.y && snakeArr[0].x===food.x){
+    foodSound.play();
+     score += 1;
+     scoreBox.innerHTML="Score: "+score;
+    snakeArr.unshift({x:snakeArr[0].x + inputDir.x, y:snakeArr[0].y +inputDir.y});
+   let a=2;
+   let b=16;
+   food={x:Math.round(a+(b-a)*Math.random()),y:Math.round(a+(b-a)*Math.random())}
+}
+ // Moving the snake
+  for(let i= snakeArr.length-2;i>=0;i--){
+    snakeArr[i+1]={...snakeArr[i]};
+  }
+      snakeArr[0].x+=inputDir.x;
+      snakeArr[0].y+=inputDir.y;
+
+    // part 2: display the snake and food
+    // display snake
+    board.innerHTML ="";
+    snakeArr.forEach((e,index)=>{                  // snake head
+        snakeElement =document.createElement('div');
+        snakeElement.style.gridRowStart=e.y;
+        snakeElement.style.gridColumnStart=e.x;
+        if(index===0){
+            snakeElement.classList.add('head');
+        }
+        else{
+            snakeElement.classList.add('snake');
+        }
+        board.appendChild(snakeElement);
+    })
+    // display food
+        
+    foodElement =document.createElement('div');
+    foodElement.style.gridRowStart=food.y;
+    foodElement.style.gridColumnStart=food.x;
+    foodElement.classList.add('food');
+    board.appendChild(foodElement);
+}
+
+// main logic starts here
+musicSound.play();
+window.requestAnimationFrame(main);
+window.addEventListener('keydown',e=>{
+    inputDir={x:0,y:1} //start the game
+    moveSound.play();
+    musicSound.play();
+     switch(e.key){
+        case "ArrowUp":
+            console.log("ArrowUp");
+            inputDir.x= 0;
+            inputDir.y= -1;
+        break;
+        case "ArrowDown":
+            console.log("ArrowDown");
+            inputDir.x= 0;
+            inputDir.y= 1;
+        break;
+        case "ArrowLeft":
+            console.log("ArrowLeft");
+            inputDir.x= -1;
+            inputDir.y= 0;
+        break;
+        case "ArrowRight":
+            console.log("ArrowRight");
+            inputDir.x= 1 ;
+            inputDir.y= 0;
+        break;
+        default:
+        break;
+     }
+     
+})
